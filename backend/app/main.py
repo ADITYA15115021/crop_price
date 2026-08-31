de7a7ba0db1151dce01f05ebd118b5375db40e40
ADITYA KAMBOJ <sierra.phoenix1511@gmail.com>
2026-08-31 12:30:30 +0530

    added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174","http://localhost:5173", ],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://crop-price-theta.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
